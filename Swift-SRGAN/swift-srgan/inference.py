6ec5dc04b64d3f087d4a3d02196121f79bfb5110
gregory scafarto <gregoryscafarto@localhost.localdomain>
2023-07-19 21:00:37 +0200

    swift sr model

---
+++ Swift-SRGAN/swift-srgan/inference.py
@@ -0,0 +1,45 @@
+import os
+import math
+import pandas as pd
+import torch
+import torchvision
+from data import TrainDataset, ValDataset, display_transform
+from torch.utils.data import DataLoader
+from models import Generator, Discriminator
+from loss import GeneratorLoss
+from metric import ssim
+from tqdm import tqdm
+import argparse
+import matplotlib.pyplot as plt 
+import cv2
+from torchvision import transforms
+import numpy as np
+
+torch.backends.cudnn.benchmark = True
+torch.cuda.manual_seed_all(42)
+
+
+if __name__ == "__main__" :
+    UPSCALE_FACTOR = 4
+    DEVICE = torch.device("cpu")
+    netG = Generator(upscale_factor=UPSCALE_FACTOR).to(DEVICE)
+    checkpoint = torch.load("./checkpoints/netG_4x_epoch100.pth.tar")
+    netG.load_state_dict(checkpoint['model'])
+
+    gt_image = cv2.imread("../dataset/test.png").astype(np.float32) / 255.
+    gt_image = cv2.cvtColor(gt_image, cv2.COLOR_BGR2RGB)
+    print(gt_image.shape)
+    gt_image =  cv2.resize(gt_image, (int(gt_image.shape[0]/8),int(gt_image.shape[1]/8)), cv2.INTER_CUBIC)
+    
+    plt.imshow(gt_image)
+    plt.savefig('demo_real.png', bbox_inches='tight')
+    gt_tensor = transforms.ToTensor()(gt_image).unsqueeze(0).to(DEVICE)
+
+
+
+    sr_img = netG(gt_tensor)
+
+    sr_image = transforms.ToPILImage()(sr_img.squeeze().detach().cpu())
+
+    plt.imshow(sr_image)
+    plt.savefig('demo_upgrade.png', bbox_inches='tight')
